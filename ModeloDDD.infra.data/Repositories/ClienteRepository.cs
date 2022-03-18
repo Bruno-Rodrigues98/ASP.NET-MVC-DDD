6b68866a5890d5cadb13756cdc0ef8b2b52e3e3c
bruno-silva98 <bro.rodrigues21@gmail.com>
2022-03-18 16:02:57 -0300

    Atualização

--- ModeloDDD.infra.data/Repositories/ClienteRepository.cs
+++ ModeloDDD.infra.data/Repositories/ClienteRepository.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using ModeloDDD.domain.entities;
+using ModeloDDD.domain.interfaces;
+using System;
 
 namespace ModeloDDD.infra.data.Repositories
 {
-    public class ClienteRepository : RepositoryBase<Cliente>
+    public class ClienteRepository : RepositoryBase<Cliente>, IClienteRepository
     {
     }
 }
